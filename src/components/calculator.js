a1b3b9b7bac05560f90390f739b0fd7e48818208
Marc Raphael <marcraphael979@gmail.com>
2021-11-01 16:40:07 +0100

    components/calculator:  Create the event function

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -4,11 +4,21 @@ import React from 'react';
 import calculate from './logic/calculate.js';
 
 // Create an obect to make every operations
-class MakeOperation extends React.Component {
+export default class MakeOperation extends React.Component {
   constructor(props) {
     super(props);
 
     // Create a state to store the value of the input
     this.state = {};
+  }
+
+  // Function to make the operation
+  eventHandler(event) {
+    // Get the button value when the user click
+    const btn = event.target.value;
+
+    // Does the operation
+    const operation = calculate(this.state, btn);
+    this.setState(operation);
   }
 }
