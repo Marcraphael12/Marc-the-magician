6a5ea48d5079b1bfe4ea5b3c491032ee7585808f
Marc Raphael <marcraphael979@gmail.com>
2021-11-01 17:28:55 +0100

    components/calculator:  Add the render function: show content

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -21,4 +21,42 @@ export default class MakeOperation extends React.Component {
     const operation = calculate(this.state, btn);
     this.setState(operation);
   }
+
+  // The render function
+  render() {
+    // Our buttons name list
+    const buttons = ['AC', '+/-', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+
+    const { next, operation, total } = this.state;
+    let result = '';
+
+    /* If the operation is not empty, we will show the operation
+     * and the total of the operation in the screen
+    */
+    if (total) {
+      result = `${total} ${operation || ''} ${next || ''}`;
+    } else if (next) {
+      result = next;
+    }
+    return (
+      <div>
+        <ul>
+          <li className="screen">{result}</li>
+          <li>
+          {buttons.map((btn) => (<button key={btn} className = 'pad' className={function giveClass(btn) {
+            if (btn === '+' || btn === 'x' || btn === '-' || btn === '/' || btn === '=') {
+              return 'different';
+            }
+            if (btn === '0') {
+              return 'zero';
+            }
+            return '';
+          }}
+          onClick={this.eventHandler}
+          type="button">{btn}</button>))}
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
