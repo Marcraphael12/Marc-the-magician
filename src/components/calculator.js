b8c58abe81195ccd69ef139d0307ab290da5fd6b
Marc Raphael <marcraphael979@gmail.com>
2021-11-01 21:28:42 +0100

    components/calculator: Update the class name, the event and the render function

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,60 +1,48 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable class-methods-use-this */
 import React from 'react';
 import calculate from './logic/calculate.js';
 
-// Create an obect to make every operations
-export default class MakeOperation extends React.Component {
+export default class MakeOperations extends React.Component {
   constructor(props) {
     super(props);
-
-    // Create a state to store the value of the input
     this.state = {};
   }
 
-  // Function to make the operation
-  eventHandler(event) {
-    // Get the button value when the user click
-    const btn = event.target.value;
+  eventhandler = (e) => {
+    const b = e.target.textContent;
+    const calc = calculate(this.state, b);
+    this.setState(calc);
+  };
 
-    // Does the operation
-    const operation = calculate(this.state, btn);
-    this.setState(operation);
-  }
-
-  // The render function
   render() {
-    // Our buttons name list
-    const buttons = ['AC', '+/-', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+    const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x',
+      '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
-    const { next, operation, total } = this.state;
+    const { next, total, operation } = this.state;
     let result = '';
-
-    /* If the operation is not empty, we will show the operation
-     * and the total of the operation in the screen
-    */
     if (total) {
       result = `${total} ${operation || ''} ${next || ''}`;
     } else if (next) {
       result = next;
     }
+
+    function addClass(button) {
+      if (button === '+' || button === 'x' || button === '-' || button === '÷' || button === '=') {
+        return 'different';
+      }
+      if (button === '0') {
+        return 'zero';
+      }
+      return '';
+    }
     return (
       <div>
         <ul>
-          <li className="screen">{result}</li>
-          <li>
-          {buttons.map((btn) => (<button key={btn} className = 'pad' className={function giveClass(btn) {
-            if (btn === '+' || btn === 'x' || btn === '-' || btn === '/' || btn === '=') {
-              return 'different';
-            }
-            if (btn === '0') {
-              return 'zero';
-            }
-            return '';
-          }}
-          onClick={this.eventHandler}
-          type="button">{btn}</button>))}
-          </li>
+          <li className='screen'>
+              <h3>
+                {result}
+              </h3>
+            </li>
+            {buttons.map((btn) => (<li className={`pad ${addClass(btn)}`} key={btn}><button key={btn} onClick={this.eventhandler} type="button">{btn}</button></li>))}
         </ul>
       </div>
     );
