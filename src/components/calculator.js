ffbad45eb9d645747a12cd8b7eb978d0e6af557b
Marc Raphael <marcraphael979@gmail.com>
2021-11-04 15:22:06 +0100

    Convert to function

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from './logic/calculate.js';
 
-export default class MakeOperations extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+export default function MakeOperations (){
+  
 
   eventhandler = (e) => {
     const b = e.target.textContent;
@@ -13,38 +10,36 @@ export default class MakeOperations extends React.Component {
     this.setState(calc);
   };
 
-  render() {
-    const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x',
-      '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x',
+    '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
-    const { next, total, operation } = this.state;
-    let result = '';
-    if (total) {
-      result = `${total} ${operation || ''} ${next || ''}`;
-    } else if (next) {
-      result = next;
-    }
-
-    function addClass(button) {
-      if (button === '+' || button === 'x' || button === '-' || button === '÷' || button === '=') {
-        return 'different';
-      }
-      if (button === '0') {
-        return 'zero';
-      }
-      return '';
-    }
-    return (
-      <div>
-        <ul>
-          <li className='screen'>
-              <h3>
-                {result}
-              </h3>
-            </li>
-            {buttons.map((btn) => (<li className={`pad ${addClass(btn)}`} key={btn}><button key={btn} onClick={this.eventhandler} type="button">{btn}</button></li>))}
-        </ul>
-      </div>
-    );
+  const { next, total, operation } = this.state;
+  let result = '';
+  if (total) {
+    result = `${total} ${operation || ''} ${next || ''}`;
+  } else if (next) {
+    result = next;
   }
+
+  function addClass(button) {
+    if (button === '+' || button === 'x' || button === '-' || button === '÷' || button === '=') {
+      return 'different';
+    }
+    if (button === '0') {
+      return 'zero';
+    }
+    return '';
+  }
+  return (
+    <div>
+      <ul>
+        <li className='screen'>
+            <h3>
+              {result}
+            </h3>
+          </li>
+          {buttons.map((btn) => (<li className={`pad ${addClass(btn)}`} key={btn}><button key={btn} onClick={this.eventhandler} type="button">{btn}</button></li>))}
+      </ul>
+    </div>
+  );
 }
