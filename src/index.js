1401e4536184c643dee9ce737894268fddfcbba4
Marc Raphael <marcraphael979@gmail.com>
2021-11-12 01:53:15 +0100

    Fix error: Delete basis CRA test files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.js';
-import reportWebVitals from './reportWebVitals.js';
 
 ReactDOM.render(
     <BrowserRouter>
@@ -12,5 +11,3 @@ ReactDOM.render(
     </BrowserRouter>,
   document.getElementById('root'),
 );
-
-reportWebVitals();
